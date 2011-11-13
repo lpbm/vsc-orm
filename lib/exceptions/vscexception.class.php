d6ab43dd44857654c0f8fb3365069b98afabb36f
Marius Orcsik <marius@habarnam.ro>
2011-11-13 23:14:00 +0200

    - removed the exceptions in favour of the ones in vsc

--- lib/exceptions/vscexception.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-class vscException extends Exception {}
--- res/config.inc.php
+++ res/config.inc.php
@@ -1,9 +1,9 @@
 <?php
-if (!defined ('VSC_LIB_PATH'))
-	define ('VSC_LIB_PATH', VSC_PATH . 'lib' . DIRECTORY_SEPARATOR);
+if (!defined ('ORM_LIB_PATH'))
+	define ('ORM_LIB_PATH', ORM_PATH . 'lib' . DIRECTORY_SEPARATOR);
 
-if (!defined ('VSC_RES_PATH'))
-	define ('VSC_RES_PATH', VSC_PATH . 'res' . DIRECTORY_SEPARATOR);
+if (!defined ('ORM_RES_PATH'))
+	define ('ORM_RES_PATH', ORM_PATH . 'res' . DIRECTORY_SEPARATOR);
 
-if (!defined ('VSC_TEST_PATH'))
-	define ('VSC_TEST_PATH', VSC_PATH . 'unit_testing' . DIRECTORY_SEPARATOR);
+if (!defined ('ORM_TEST_PATH'))
+	define ('ORM_TEST_PATH', ORM_PATH . 'unit_testing' . DIRECTORY_SEPARATOR);
