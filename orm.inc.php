761b9a87eb63c020dab9679d36a26faaa2f62db5
Marius Orcsik <marius@habarnam.ro>
2011-11-14 00:09:33 +0200

    - removed other unnecessary classes from orm
    * modified the loader to throw an exception when vsc is not present

---
+++ orm.inc.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined ('ORM_PATH')) {
+	define ('ORM_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+}
+
+
+require (ORM_PATH . 'res'. DIRECTORY_SEPARATOR .'config.inc.php');
+require (ORM_PATH . 'res'. DIRECTORY_SEPARATOR .'functions.inc.php');
+
+if (!defined ('ROOT_MAIL')) {
+	if (!isCli()) {
+		define ('ROOT_MAIL', 'root@' . $_SERVER['HTTP_HOST']);
+	} else {
+		define ('ROOT_MAIL', 'root@localhost');
+	}
+}
+
+if ( defined (VSC_PATH) ) {
+	$sMessage = 'libVSC is required in order to load this ORM framework';
+	throw new ErrorException ($sMessage, E_USER_ERROR);
+} else {
+	include (VSC_PATH . 'vsc.inc.php');
+	
+	import (ORM_LIB_PATH);
+	import (ORM_RES_PATH);
+}
